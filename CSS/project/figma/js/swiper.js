6e2a5dafdc19c19175f9b8335b018fa99da8a70d
Java-zhara <java-zhara@mail.ru>
2021-03-01 15:30:31 +0300

    figmaEnd

--- CSS/project/figma/js/swiper.js
+++ CSS/project/figma/js/swiper.js
@@ -1,14 +1,47 @@
-  
-    new Swiper('.image-slider',{
+let catalogSlider = null;
+const mediaQuerySize = 767;
 
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev'
-    
+function catalogSliderInit() {
+  if (!catalogSlider) {
+    catalogSlider = new Swiper(".image-slider", {
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+
+      breakpoints: {
+        320: {
+          slidesPerView: 1,
         },
-    
-        pagination: {
-            el: '.swiper-pagination-fraction',
-            type: 'fraction',
+        600: {
+          slidesPerView: 2,
         },
+      },
     });
+  }
+}
+
+function catalogSliderDestroy() {
+  if (catalogSlider) {
+    catalogSlider.destroy();
+    catalogSlider = null;
+  }
+}
+
+// Если ширина экрана меньше или равна mediaQuerySize(767)
+if (window.innerWidth <= mediaQuerySize) {
+  // Инициализировать слайдер
+  catalogSliderInit();
+} else {
+  // Уничтожить слайдер
+  catalogSliderDestroy();
+}
+
+// те же действия при изменении ширины экрана
+window.addEventListener("resize", () => {
+  if (window.innerWidth <= mediaQuerySize) {
+    catalogSliderInit();
+  } else {
+    catalogSliderDestroy();
+  }
+});
