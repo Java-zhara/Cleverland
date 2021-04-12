2f40b2d6d5715e63fbfdf9bea149d667d4fa6cfc
Java-zhara <java-zhara@mail.ru>
2021-04-12 16:06:49 +0300

    lesson16js

---
+++ JavaScript/lessons/lesson16/1/setInterval.js
@@ -0,0 +1,23 @@
+'use strict';
+const div = document.querySelectorAll('div');
+
+const a = +prompt('Введите начало отсчета', 5);
+
+const b = +prompt('Введите конец отсчета', 0);
+
+const printNumbers = (from, to) => {
+
+    let timerId = setInterval(() => {
+        div[0].innerHTML = `Отсчет пошел: ${from}`;
+        if (from == to) {
+            clearInterval(timerId);
+        } else if (a <= b) {
+            div[0].innerHTML = `Введите первую цифру больше второй`;
+        } else if (isNaN(from) || isNaN(to)) {
+            div[0].innerHTML = `Введите цифры`;
+        };
+        from--;
+    }, 1000);
+}
+
+printNumbers(a, b);
